d8b6344d2a806a5de8f335b0ef618299ff030d86
gtopacio <geoff_topacio@dlsu.edu.ph>
2023-05-24 20:19:02 +0800

    Dashboard

---
+++ admin-panel/src/data/books.js
@@ -0,0 +1,28 @@
+export const books = [
+    {
+        title: "When Blood Meets Earth",
+        author: "E.A. Noble",
+        synopsis:
+            "Synompys kuno pero mema lang to kase wala akong maisip ilagay daming white space so dadamihan ko pa ang kamemahan kase dami pang white space",
+        img: "/books/book1.jpg",
+        genre: "Fiction"
+    },
+    {
+        title: "When Blood Meets Earth",
+        author: "E.A. Noble",
+        synopsis:
+            "Synompys kuno pero mema lang to kase wala akong maisip ilagay daming white space so dadamihan ko pa ang kamemahan kase dami pang white space",
+        img: "/books/book1.jpg",
+        status: "Borrowed",
+        genre: "Fiction"
+    },
+    {
+        title: "When Blood Meets Earth",
+        author: "E.A. Noble",
+        synopsis:
+            "Synompys kuno pero mema lang to kase wala akong maisip ilagay daming white space so dadamihan ko pa ang kamemahan kase dami pang white space",
+        img: "/books/book1.jpg",
+        status: "Available",
+        genre: "Fiction"
+    }
+];
--- admin-panel/src/pages/dashboard.js
+++ admin-panel/src/pages/dashboard.js
@@ -1,10 +1,14 @@
-import BasePage from "@/components/BasePage";
+import BookCard from "@/components/BookCard";
+import DashboardHistory from "@/components/DashboardHistory";
 import { AiOutlineSearch } from "react-icons/ai";
+import Navigation from "@/components/Navigation";
+import { books } from "@/data/books";
 
 export default function dashboard() {
     return (
-        <BasePage>
-            <div className="flex h-full w-full flex-col items-center justify-center align-middle">
+        <>
+            <Navigation />
+            <div className="my-24 flex h-full w-full flex-col items-center justify-center space-y-12 align-middle">
                 <div className="relative mb-2 w-1/2">
                     <input
                         className="h-10 w-full rounded-lg border bg-white pl-3 pr-8 text-base focus:outline-none"
@@ -16,8 +20,28 @@ export default function dashboard() {
                         <AiOutlineSearch color="black" />
                     </div>
                 </div>
-                
+                <div className="grid w-10/12 grid-cols-3 gap-4">
+                    {books.map((book) => (
+                        <BookCard {...book} />
+                    ))}
+                </div>
+                <div className="flex w-10/12 flex-col text-black">
+                    <h1 className="text-2xl font-bold">Latest Borrowed:</h1>
+                    <div className="grid grid-cols-3 place-content-center">
+                        <div></div>
+                        <div className="flex w-full flex-row items-center justify-center align-middle">
+                            <p>Category</p>
+                        </div>
+
+                        <div className="flex w-full flex-row items-center justify-center align-middle">
+                            <p>Status</p>
+                        </div>
+                        {books.map((book) => (
+                            <DashboardHistory {...book} />
+                        ))}
+                    </div>
+                </div>
             </div>
-        </BasePage>
+        </>
     );
 }
