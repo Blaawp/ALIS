b42ea77edad38210da2625dc69a80bd8524374f5
Joric <joric_bernardo_batac@dlsu.edu.ph>
2023-05-24 20:53:25 +0800

    set main activity to login

--- alismobile/app/src/main/java/com/example/alis_mobile/MainActivity.kt
+++ alismobile/app/src/main/java/com/example/alis_mobile/MainActivity.kt
@@ -15,17 +15,7 @@ import com.example.alis_mobile.ui.theme.AlismobileTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AlismobileTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.login_screen)
     }
 }
 
