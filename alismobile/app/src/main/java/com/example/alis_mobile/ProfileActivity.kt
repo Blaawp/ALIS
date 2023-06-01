95237bafe232567c5639e69640df1e0f85152604
Joric <joric_bernardo_batac@dlsu.edu.ph>
2023-06-01 18:57:27 +0800

    clickable stuff

--- alismobile/app/src/main/java/com/example/alis_mobile/ProfileActivity.kt
+++ alismobile/app/src/main/java/com/example/alis_mobile/ProfileActivity.kt
@@ -3,6 +3,7 @@ package com.example.alis_mobile
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import android.widget.Button
 import android.widget.ImageView
@@ -16,15 +17,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.alis_mobile.ui.theme.AlismobileTheme
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class RuleActivity : ComponentActivity() {
+class ProfileActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.rule_screen)
+        setContentView(R.layout.profile_screen)
 
         val homeNav: ImageView = findViewById(R.id.homeNav)
         val compassNav: ImageView = findViewById(R.id.compassNav)
         val profNav: ImageView = findViewById(R.id.profNav)
+        val expCont: ImageView = findViewById(R.id.profileCard)
+
+        val groupedLayout: LinearLayout = findViewById(R.id.groupedLayout)
 
         homeNav.setOnClickListener {
             // Handle homeNav click event
@@ -46,5 +50,16 @@ class RuleActivity : ComponentActivity() {
             val intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
         }
+
+        expCont.setOnClickListener {
+            if (groupedLayout.visibility == View.VISIBLE) {
+                // Elements are currently visible, so hide them
+                groupedLayout.visibility = View.GONE
+            } else {
+                // Elements are currently hidden, so show them
+                groupedLayout.visibility = View.VISIBLE
+            }
+        }
+
     }
 }
