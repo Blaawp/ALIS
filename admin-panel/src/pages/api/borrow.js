bc2cc2ef123d14fbb3e062a320f7b97bc39c5a4b
RJ-Csc <rolan_cosico@dlsu.edu.ph>
2023-06-02 10:11:28 +0800

    Borrow backend

--- admin-panel/src/pages/api/borrow.js
+++ admin-panel/src/pages/api/borrow.js
@@ -1,4 +1,26 @@
+const mysql = require('mysql2');
+
 export default function handler(req, res) {
-    res.status(200).json({ hehe: 'hehe' })
-  }
+
+    // const connection = mysql.createConnection({
+    //     host: 'aws.connect.psdb.cloud',
+    //     user: 'usl35jqqhwy7mmkkpsyr',
+    //     database: process.env.DATABASE_URL
+    // });
+
+    // connection.query(
+    //     'SELECT * FROM `users`',
+    //     function(err, results, fields) {
+    //         console.log(results);
+    //         console.log(fields);
+    //     }
+    // );
+
+
+    const { bookCode, studentId } = JSON.parse(req.body);
+
+    let message = "Book Code: " + bookCode + " Student ID: " + studentId;
+
+    res.status(200).json({ message: message });
+}
   
